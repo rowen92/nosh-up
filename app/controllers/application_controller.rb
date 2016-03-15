ea82c9ce206246d6d7e192ecc61eedcb8648266f
rowen92 <maksym.pakhn@gmail.com>
2016-03-15 18:43:15 +0200

    Cart added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_cart
 
   private
 
@@ -23,6 +24,14 @@ class ApplicationController < ActionController::Base
         flash[:alert] = "Эта функция доступна только администратору!"
         redirect_to root_path
       end
+    end
+
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart
+      cart
     end
 
     helper_method :current_user
---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,16 @@
+class CartsController < ApplicationController
+
+  def show
+    @cart = Cart.find(session[:cart_id])
+    # @line_items = @cart.line_items
+    # @order = Order.new
+  end
+
+  def destroy
+    @cart = Cart.find(session[:cart_id])
+    @cart.destroy
+    flash[:success] = "Корзина пустая."
+    redirect_to root_path
+  end
+
+end
