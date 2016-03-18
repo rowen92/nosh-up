b87811ed4646e262f65104850e742acce1416e36
rowen92 <maksym.pakhn@gmail.com>
2016-03-18 18:40:17 +0200

    style cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :current_cart, :set_categories
+  before_action :current_cart, :set_categories, :line_items_count
 
   private
+
+    def line_items_count
+      @line_items_count = current_cart.line_items.count
+    end
 
     def set_categories
       @categories = Category.all.order(:title)
