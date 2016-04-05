0d6ff1ac0fc70820800fdbcab2a120e3af79b8f2
rowen92 <maksym.pakhn@gmail.com>
2016-04-05 14:36:23 +0300

    edited mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,10 +81,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'nosh-up.heroku.com',
+    domain:               'gmail.com',
     user_name:            'maksym.pakhn.rails',
     password:             'maksym.pakhn.rails.',
     authentication:       'plain',
-    enable_starttls_auto: true  }
+    enable_starttls_auto: true,
+    openssl_verify_mode:  'none'  }
 
 end
